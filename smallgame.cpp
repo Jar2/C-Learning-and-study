26a543178632cba0a7b250586603939cc444b173
MisterJar <whitehousejared1@gmail.com>
2017-09-12 19:30:33 -0400

    Added values to all of the structs not for the village

--- smallgame.cpp
+++ smallgame.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>
+#include <cstdio>
 using namespace std;
 /*
  * this game is going to be a small adventure game in the terminal where the player
@@ -17,31 +17,101 @@ using namespace std;
 //
 int main()
 {
-
+	// make the items that the forest is going to use! like whatever!
+	struct Village
+	{
+		int buyer;
+		int seller;
+		int blacksmith;
+		int person;
+		int pesent;
+		int cook;
+		int bucther;
+		int police;
+		int gaurd;
+		int house;
+		int castle;
+		int dungeon;
+		int caves;
+	};
 	struct Forest
 	{
-	//	cout << "using the goto statement!\n";
-	//	cout << "Welcome to this place! please expolore now!\n";
+		int lake;
+		int talltrees;
+		int bears;
+		int ants;
+		int leaves;
 	};
 
 	struct Plains
 	{
-	//	cout << "using the goto statement!\n";
-	//	cout << "Welcome to this place! please expolore now!\n";
+		int river;
+		int wheat;
+		int village;
+		int grass;
+		int farmer;
 	};
 	struct Desert
 	{
-	//	cout << "using the goto statement!\n";
-	//	cout << "Welcome to this place! please expolore now!\n";
+		int desertSand;
+		int catus;
+		int deadbushes;
+		int skull;
+	
 	};
 	struct Beach
 	{
-	//	cout << "using the goto statement!\n";
-	//	cout << "Welcome to this place! please expolore now!\n";
+		int sand;
+		int beachball;
 	};
+	struct Player
+	{
+		int health;
+		int armour;
+		int mana;
+	};
+	/* making the definitions of the objects in the world with structs so you can use them
+	 *
+	 * TODO for the objects in the game or adventure
+	 *
+	 */
+	// player!
+	struct Player Player1;
+	Player1.health = 100;
+	Player1.armour = 20;
+	Player1.mana   = 0;
+	//TODO for objects.
+	//For the beach section
+	struct Beach Beachobj1;
+	Beachobj1.sand = 0.5;
+	Beachobj1.beachball = 0;
+	// The forest
+	struct Forest Forestobj1;
+	Forestobj1.ants = 0;
+	Forestobj1.leaves = 0;
+	//The Plains
+	struct Plains Plains1;
+	Plains1.wheat = 20;
+	Plains1.grass = 0;
+	//The desert
+	struct Desert Desertobj1;
+	Desertobj1.desertSand = 0.5;
+	Desertobj1.catus = 10;
+	Desertobj1.deadbushes = 0.1;
+	Desertobj1.skull = 5;
+	// The Village
+	// since they are all people and living things, or places.
+	// I think i should make them their own class and functions
+	//
+	// TODO
+
+	/*
+	 * MAKE AN INVENTORY SYSTEM!!!!
+	 */
 	// about the player or what ever!
+	string mystr; // you need the string there to enter in someones name aka MisterJar
 	cout << "Welcome to the game, Press any key to continue: " << endl;
-	string mystr;
+	// the game what is the players name is going to be;
 	cout << "what is your name: ";
 	getline(cin, mystr);
 	cout << "Hello " << mystr << endl;
@@ -71,16 +141,16 @@ int main()
 			cout << "You are now in the desert! welcome! " << endl;
 			break;
 		case 'F':
-// This is going to make the player go to the forest!
+			// This is going to make the player go to the forest!
 			cout << "welcome to the forest! " << endl;
 			break;
 
 		case 'P':
-// This is going to make the player go to the plains!
+			// This is going to make the player go to the plains!
 			cout << "wlecome to the plains" << endl;
 			break;
 		case 'B':
-// This is going to make the player go to the beach!
+			// This is going to make the player go to the beach!
 			cout << "wlecome to the beach" << endl;
 			break;
 			}
@@ -88,4 +158,3 @@ int main()
 	}
 	return 0;
 }
-	
